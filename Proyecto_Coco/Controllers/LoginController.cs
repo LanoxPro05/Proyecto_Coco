13b472cb5904e518d7bbb18fcb4e0954ee64148a
eme503 <emersongranados41@gmail.com>
2023-01-29 17:29:16 -0600

    Completado

---
+++ Proyecto_Coco/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Coco.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+        public ActionResult Registrar()
+        {
+            return View();
+        }
+    }
+}
