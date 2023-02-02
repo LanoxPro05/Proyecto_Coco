1136831f90ba7d44dde5017e5ff8da814ae59841
eme503 <emersongranados41@gmail.com>
2023-02-01 18:41:30 -0600

    01/02/2023

---
+++ Proyecto_Coco/Controllers/HomeMyDogController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Coco.Controllers
+{
+    public class HomeMyDogController : Controller
+    {
+        // GET: HomeMyDog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
